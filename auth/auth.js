49dc609ffaaf113edb7796564cbf3415c769f416
SebastienLaurentt <slaurent.26@gmail.com>
2024-08-02 16:38:57 +0200

    add auth on rando add, update and delete

--- auth/auth.js
+++ auth/auth.js
@@ -1,6 +1,5 @@
-// auth/auth.js
 const jwt = require('jsonwebtoken');
-const privateKey = require('./private_key'); // Assurez-vous que ce fichier contient votre clé privée JWT
+const privateKey = require('./private_key'); // Vérifiez le chemin du fichier
 
 module.exports = (req, res, next) => {
   const authorizationHeader = req.headers.authorization;
@@ -11,11 +10,16 @@ module.exports = (req, res, next) => {
   }
 
   const token = authorizationHeader.split(' ')[1];
+  console.log('Token reçu :', token);
+
   jwt.verify(token, privateKey, (error, decodedToken) => {
     if (error) {
+      console.error('Erreur lors de la vérification du token :', error);
       const message = `L'utilisateur n'est pas autorisé à accèder à cette ressource.`;
       return res.status(401).json({ message, data: error });
     }
+
+    console.log('Token décodé :', decodedToken);
 
     const userId = decodedToken.userId;
     if (req.body.userId && req.body.userId !== userId) {
