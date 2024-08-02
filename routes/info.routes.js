0568460098bfca86859bef90ad2b4d15afe7c34d
SebastienLaurentt <slaurent.26@gmail.com>
2024-08-02 20:11:46 +0200

    add auth on info delete, update and add

--- routes/info.routes.js
+++ routes/info.routes.js
@@ -1,12 +1,18 @@
-const { allInfos, addInfo, deleteInfo, updateInfo, getInfoById } = require("../controllers/info.controller");
-
+const {
+  allInfos,
+  addInfo,
+  deleteInfo,
+  updateInfo,
+  getInfoById,
+} = require("../controllers/info.controller");
+const auth = require("../auth/auth");
 const router = require("express").Router();
 
 router.get("/api/infos", allInfos);
 router.post("/api/infos", addInfo);
-router.delete("/api/infos/:id", deleteInfo);
-router.put("/api/infos/:id", updateInfo);
-router.get("/api/infos/:id", getInfoById); 
 
+router.delete("/api/infos/:id", auth, deleteInfo);
+router.put("/api/infos/:id", auth, updateInfo);
+router.get("/api/infos/:id", auth, getInfoById);
 
 module.exports = router;
--- routes/rando.routes.js
+++ routes/rando.routes.js
@@ -1,5 +1,4 @@
-const express = require("express");
-const auth = require('../auth/auth');  // Importez votre middleware d'authentification
+const auth = require("../auth/auth");
 const {
   allRandos,
   addRando,
@@ -8,15 +7,13 @@ const {
   getRandoById,
 } = require("../controllers/rando.controller");
 
-const router = express.Router();
+const router = require("express").Router();
 
+router.get("/api/randos", allRandos);
+router.get("/api/randos/:id", getRandoById);
 
-router.get("/api/randos", allRandos); 
-router.get("/api/randos/:id", getRandoById); 
-
-
-router.post("/api/randos", auth, addRando); 
-router.delete("/api/randos/:id", auth, deleteRando); 
-router.put("/api/randos/:id", auth, updateRando); 
+router.post("/api/randos", auth, addRando);
+router.delete("/api/randos/:id", auth, deleteRando);
+router.put("/api/randos/:id", auth, updateRando);
 
 module.exports = router;
