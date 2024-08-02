ed57e5e6a31729a496745e751f8f778bed7c5387
SebastienLaurentt <slaurent.26@gmail.com>
2024-08-02 19:02:14 +0200

    update auth with logs

--- auth/auth.js
+++ auth/auth.js
@@ -6,19 +6,22 @@ module.exports = (req, res, next) => {
 
   if (!authorizationHeader) {
     const message = `Vous n'avez pas fourni de jeton d'authentification. Ajoutez-en un dans l'en-tête de la requête.`;
+    console.log(message); // Log pour débogage
     return res.status(401).json({ message });
   }
   
   const token = authorizationHeader.split(' ')[1];
-  console.log('Token reçu:', token);  
+  console.log('Token reçu:', token); // Log pour débogage
 
   jwt.verify(token, privateKey, (error, decodedToken) => {
     if (error) {
       const message = `L'utilisateur n'est pas autorisé à accéder à cette ressource.`;
+      console.log(message, error); // Log pour débogage
       return res.status(401).json({ message, data: error });
     }
 
-    req.userId = decodedToken.userId; 
+    console.log('Token décodé:', decodedToken); // Log pour débogage
+    req.userId = decodedToken.userId;
     next();
   });
 };
